00a9762e18e2b99d1b5529b086c96b9c69f3cb61
Chadrack Mbuyi <chadrackmbuyi2@gmail.com>
2022-09-28 08:08:15 +0200

    Completed my controller test case

---
+++ src/test/java/za/ac/cput/controller/EmployeeControllerTest.java
@@ -0,0 +1,87 @@
+package za.ac.cput.controller;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import za.ac.cput.entity.Employee;
+import za.ac.cput.factory.EmployeeFactory;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class EmployeeControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired private EmployeeController controller;
+    @Autowired private TestRestTemplate restTemplate;
+
+    private Employee employee;
+    private String baseUrl;
+
+    @BeforeEach
+    void setUp() {
+        assertNotNull(controller);
+        this.employee = EmployeeFactory
+                .build("test-fname","test-lname","test-address");
+        this.baseUrl = "http://localhost:" + this.port + "/restaurant/employee/";
+    }
+
+    @Test
+    @Order(1)
+    void save() {
+        String url = baseUrl + "save";
+        System.out.println(url);
+        ResponseEntity<Employee> response = this.restTemplate
+                .postForEntity(url,this.employee,Employee.class);
+        System.out.println(response);
+        assertAll(
+                () -> assertEquals(HttpStatus.OK, response.getStatusCode()),
+                () -> assertNotNull(response.getBody())
+        );
+    }
+
+    @Test
+    @Order(2)
+    void read() {
+        String url = baseUrl + "read/" + this.employee.getEmpId();
+        System.out.println(url);
+        ResponseEntity<Employee> response = this.restTemplate.getForEntity(url, Employee.class);
+        System.out.println(response);
+        assertAll(
+                //Expected Not found since we are reading something that never exist
+                () -> assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode())
+        );
+    }
+
+    @Test
+    @Order(3)
+    void delete() {
+        String url = baseUrl + "delete/" + this.employee.getEmpId();
+        this.restTemplate.delete(url);
+   }
+
+    @Test
+    @Order(4)
+    void getAll() {
+        String url = baseUrl + "all";
+        System.out.println(url);
+        ResponseEntity<Employee[]> response =
+                this.restTemplate.getForEntity(url,Employee[].class);
+        System.out.println(Arrays.asList(response.getBody()));
+        assertAll(
+                () -> assertEquals(HttpStatus.OK, response.getStatusCode()),
+                () -> assertNotNull(response.getBody().length == 0)
+        );
+
+
+    }
+}
